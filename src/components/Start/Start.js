593bf015219d4a95ade6e02c54aad73930268b6c
Luis Fraga <luis5944@gmail.com>
2020-10-18 22:02:49 +0200

    name

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -6,7 +6,7 @@ export const Start = () => {
     <div>
       <main className="start" id="start">
         <h1 className="start-head">
-          Hola, mi nombre es <span className="start-name">Luis</span>
+          Hola, mi nombre es <span className="start-name">Luis F.N</span>
         </h1>
         <h1 className="start-body">Soy desarrollador.</h1>
         <Link to="about" smooth duration={200}>
